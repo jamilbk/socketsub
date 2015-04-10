5d86279a811babb8df5b9d8132186622e25cd5db
Jamil Bou Kheir <jamil@elbii.com>
2015-04-09 22:39:53 -0500

    first working prototype

--- app.js
+++ app.js
@@ -4,9 +4,64 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var CHANNELS = [
+  'news',
+  'btcprice',
+  'bugs'
+];
+
+/*
+ * BEGIN
+ * WebSockets Stuff
+ */
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+  , wss = new WebSocketServer({port: 8080})
+  , clients = [];
+
+// broadcast postgres notification to ws connected clients
+var wsBroadcast = function (pgResponse) {
+  console.log('pubsub message received');
+  console.log(pgResponse);
+  clients.forEach(function (client) {
+    client.send(pgResponse.payload);
+  });
+};
  
+wss.on('connection', function (ws) {
+  // add client to list of connected clients
+  clients.push(ws);
+  ws.on('close', function () {
+    // remove connected client
+    clients.splice(clients.indexOf(ws), 1);
+  });
+});
+/*
+ * END
+ * WebSockets Stuff
+ */
+
+/*
+ * BEGIN
+ * Postgresql Stuff
+ */
+var pg = require('pg');
+//                                 user      host      table
+var db = new pg.Client('postgres://socketsub@localhost/socketsub');
+db.connect(function (err) {
+  if (err) throw err;
+
+  CHANNELS.forEach(function (channel) {
+    console.log('listening on channel ' + channel);
+    db.query('LISTEN ' + channel)
+  });
+
+  db.on('notification', wsBroadcast);
+});
+/*
+ * END
+ * Postgresql Stuff
+ */
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- public/javascripts/socketpub.js
+++ public/javascripts/socketpub.js
@@ -3,5 +3,13 @@ $(document).ready(function () {
 
   var ws = new WebSocket('ws://' + window.location.hostname + ':8080');
 
-  console.log(ws);
+  ws.onmessage = function (event) {
+    // client received message from server
+    var parsed = JSON.parse(event.data)
+      , name = parsed.name
+      , data = parsed.data
+      , selector = 'tr#' + name + ' td.data';
+
+    $(selector).html(data);
+  };
 });
